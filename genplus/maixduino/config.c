083a23fac43026c2fda17ba667271e885ea62a46
h1romas4 <h1romas4@gmail.com>
2019-08-10 23:39:09 +0900

    add sound and video init

--- genplus/maixduino/config.c
+++ genplus/maixduino/config.c
@@ -34,7 +34,7 @@ void set_config_defaults(void)
   config.addr_error     = 1;
   config.bios           = 0;
   config.lock_on        = 0; /* = OFF (can be TYPE_SK, TYPE_GG & TYPE_AR) */
-  config.ntsc           = 0;
+  config.ntsc           = 3;
   config.lcd            = 0; /* 0.8 fixed point */
 
   /* display options */
--- genplus/maixduino/main.c
+++ genplus/maixduino/main.c
@@ -8,8 +8,16 @@
 #include "md_ntsc.h"
 #include "sms_ntsc.h"
 
+#define SOUND_FREQUENCY 48000
+#define SOUND_SAMPLES_SIZE  2048
+
+#define VIDEO_WIDTH  320
+#define VIDEO_HEIGHT 240
+
 md_ntsc_t *md_ntsc;
 sms_ntsc_t *sms_ntsc;
+
+static short soundframe[SOUND_SAMPLES_SIZE];
 
 struct _zbank_memory_map zbank_memory_map[256];
 
@@ -25,14 +33,27 @@ int main(void)
     sdcard_begin();
     fs_begin();
 
-    // init
+    // system init
     error_init();
     set_config_defaults();
 
+    // video ram init
+    sms_ntsc = (sms_ntsc_t *)malloc(sizeof(sms_ntsc_t));
+    md_ntsc = (md_ntsc_t *)malloc(sizeof(md_ntsc_t));
+    sms_ntsc_init(sms_ntsc, &sms_ntsc_rgb);
+    md_ntsc_init(md_ntsc, &md_ntsc_rgb);
+
+    // load rom
     load_rom("COLUMS.BIN");
+
+    // emurator init
+    audio_init(SOUND_FREQUENCY, 0);
     system_init();
 
     lcd_fill_rectangle(100, 100, 200, 200, 0xff00);
+
+    free(md_ntsc);
+    free(sms_ntsc);
 
     printf("maixduino stabled.\n");
     while(1)
